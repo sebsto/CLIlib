df6da7a2a466792b8177de97373564a784628fb6
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2025-05-16 12:22:16 +0200

    Remove support for shell invocations now that https://github.com/swiftlang/swift-subprocess exists

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         .target(
             name: "CLIlib",
             dependencies: [
-                .product(name: "Logging", package: "swift-log")
+                .product(name: "Logging", package: "swift-log"),
             ]),
         .testTarget(
             name: "CLIlibTests",
